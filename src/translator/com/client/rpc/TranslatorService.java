4efa3d151ceb2e09498a51393a7087b9006f2102
Alexander <Alexander@overmind>
2011-08-25 00:09:53 +0300

    Further implementation of Favourite.

--- src/translator/com/client/rpc/TranslatorService.java
+++ src/translator/com/client/rpc/TranslatorService.java
@@ -1,5 +1,7 @@
 package translator.com.client.rpc;
 
+import java.util.Set;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
@@ -7,5 +9,11 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 public interface TranslatorService extends RemoteService {
 
 	String translate(String word, String dictEngine) throws Exception;
+	
+	Boolean addWord(String userSecret, String word) throws Exception;
+	
+	Boolean removeWord(String userSecret, String word) throws Exception;
+	
+	Set<String> getWords(String userSecret, String filter) throws Exception;
 	
 }
--- src/translator/com/client/rpc/TranslatorServiceAsync.java
+++ src/translator/com/client/rpc/TranslatorServiceAsync.java
@@ -1,9 +1,17 @@
 package translator.com.client.rpc;
 
+import java.util.Set;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface TranslatorServiceAsync {
 
 	void translate(String word, String dictEngine, AsyncCallback<String> callback);
+
+	void addWord(String userSecret, String word, AsyncCallback<Boolean> callback);
+
+	void removeWord(String userSecret, String word, AsyncCallback<Boolean> callback);
+
+	void getWords(String userSecret, String filter, AsyncCallback<Set<String>> callback);
 
 }
